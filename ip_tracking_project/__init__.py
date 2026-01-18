5b8b867c212d60f46703fd1392846fc946459a4d
BillyMwangiDev <billymwangi200@gmail.com>
2026-01-18 22:22:59 +0300

    Fix: Make Celery import optional for free tier deployment

--- ip_tracking_project/__init__.py
+++ ip_tracking_project/__init__.py
@@ -1,3 +1,8 @@
-from .celery_app import app as celery_app
-
-__all__ = ('celery_app',)
+# Import Celery app only if celery is being used
+# This allows the app to work without Celery workers on free tier
+try:
+    from .celery_app import app as celery_app
+    __all__ = ('celery_app',)
+except ImportError:
+    # Celery not available or not needed (free tier deployment)
+    pass
